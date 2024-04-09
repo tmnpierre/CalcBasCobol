fee5987d6b6901b69ffd79eac61730045773c477
tmnpierre <tomyn.pierre@gmail.com>
2024-04-09 16:24:21 +0200

    feat(calc-basic): remove operation selection and condition checks

--- CalcBas.cbl
+++ CalcBas.cbl
@@ -15,77 +15,30 @@
 
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-      
-      *    Variables pour les opérations.
        01 WS-VARIABLES.
            05 WS-NUM1        PIC 9(5).        
            05 WS-NUM2        PIC 9(5).        
            05 WS-RESULT      PIC 9(5)V99.     
-
-      *    Gestion de l'opération choisie par l'utilisateur.
-       01 WS-OPERATION      PIC X.            
-
-      *    Définition des opérations valides.
-           88 EXIT-PROGRAM      VALUE 'E'.        
-           88 ADDITION          VALUE 'A'.        
-           88 SUBTRACTION       VALUE 'S'.        
-           88 MULTIPLICATION    VALUE 'M'.        
-           88 DIV               VALUE 'D'.        
+           05 WS-DIV-RESULT  PIC 9(5)V99 VALUE 0.
 
        PROCEDURE DIVISION.
-
        0000-MAIN-START.
-           PERFORM UNTIL EXIT-PROGRAM
+           DISPLAY "Entrer le premier nombre: " WITH NO ADVANCING
+           ACCEPT WS-NUM1
 
-               DISPLAY "Entrer le premier nombre: " WITH NO ADVANCING
-               ACCEPT WS-NUM1
+           DISPLAY "Entrer le deuxième nombre: " WITH NO ADVANCING
+           ACCEPT WS-NUM2
 
-               DISPLAY "Entrer le deuxième nombre: " WITH NO ADVANCING
-               ACCEPT WS-NUM2
+           PERFORM ADDITION-PARAGRAPH
+           PERFORM SUBTRACTION-PARAGRAPH
+           PERFORM MULTIPLICATION-PARAGRAPH
+           PERFORM DIV-PARAGRAPH
 
-               DISPLAY "Choisir l'opération: " WITH NO ADVANCING
-               ACCEPT WS-OPERATION
-
-               MOVE FUNCTION UPPER-CASE(WS-OPERATION) TO WS-OPERATION
-
-                 EVALUATE WS-OPERATION
-
-                   WHEN 'A'
-                     IF WS-NUM1 = 1 AND WS-NUM2 = 1 THEN
-                     DISPLAY "Le résultat est: 11"
-                   ELSE
-                     PERFORM ADDITION-PARAGRAPH
-                   END-IF
-
-                   WHEN 'S'
-                     PERFORM SUBTRACTION-PARAGRAPH
-
-                   WHEN 'M'
-                     PERFORM MULTIPLICATION-PARAGRAPH
-
-                   WHEN 'D'
-                     IF WS-NUM2 = 0 THEN
-                       DISPLAY "Erreur: Division par zéro."
-                     ELSE
-                       PERFORM DIV-PARAGRAPH
-                     END-IF
-
-                   WHEN 'E'
-                     CONTINUE 
-
-                   WHEN OTHER
-                     DISPLAY "Opération invalide. Veuillez réessayer."
-
-                  END-EVALUATE
-
-           END-PERFORM.
-      *    DISPLAY "Programme terminé."
+           DISPLAY "Programme terminé.".
 
        0000-MAIN-STOP.
            STOP RUN.
 
-
-      * Paragraphes des opérations mathématiques.
        ADDITION-PARAGRAPH.
            ADD WS-NUM2 TO WS-NUM1 GIVING WS-RESULT.
            DISPLAY "Le résultat de l'addition est: " WS-RESULT.
@@ -99,9 +52,5 @@
            DISPLAY "Le résultat de la multiplication est: " WS-RESULT.
 
        DIV-PARAGRAPH.
-           IF WS-NUM2 NOT = 0 THEN
-               DIVIDE WS-NUM1 BY WS-NUM2 GIVING WS-RESULT
-               DISPLAY "Le résultat de la division est: " WS-RESULT
-           ELSE
-               DISPLAY "Erreur: Division par zéro."
-           END-IF.
+           DIVIDE WS-NUM1 BY WS-NUM2 GIVING WS-DIV-RESULT
+           DISPLAY "Le résultat de la division est: " WS-DIV-RESULT.
